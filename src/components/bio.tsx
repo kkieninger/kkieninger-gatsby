1f36f837a5386b3212327f1ac051ec515601ecd5
kkieninger <ktkienin@gmail.com>
2020-12-17 15:21:22 -0500

    fix: clean up bio query

--- src/components/bio.tsx
+++ src/components/bio.tsx
@@ -12,35 +12,21 @@ import Image from "gatsby-image"
 import { rhythm } from "../utils/typography"
 
 const Bio: FC = () => {
-  // const data = useStaticQuery(graphql`
-  //   query BioQuery {
-  //     avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
-  //       childImageSharp {
-  //         fixed(width: 50, height: 50, quality: 95) {
-  //           width
-  //           height
-  //           src
-  //           srcSet
-  //           base64
-  //           tracedSVG
-  //           srcWebp
-  //           srcSetWebp
-  //         }
-  //       }
-  //     }
-  //     site {
-  //       siteMetadata {
-  //         author {
-  //           name
-  //           summary
-  //         }
-  //         social {
-  //           twitter
-  //         }
-  //       }
-  //     }
-  //   }
-  // `)
+  const data = useStaticQuery(graphql`
+    query BioQuery {
+      site {
+        siteMetadata {
+          author {
+            name
+            summary
+          }
+          social {
+            twitter
+          }
+        }
+      }
+    }
+  `)
 
   const { author, social } = data.site.siteMetadata
   return (
@@ -50,19 +36,6 @@ const Bio: FC = () => {
         marginBottom: rhythm(2.5),
       }}
     >
-      {/* <Image
-        fixed={data.avatar.childImageSharp.fixed}
-        alt={author.name}
-        style={{
-          marginRight: rhythm(1 / 2),
-          marginBottom: 0,
-          minWidth: 50,
-          borderRadius: `100%`,
-        }}
-        imgStyle={{
-          borderRadius: `50%`,
-        }}
-      /> */}
       <p>
         Written by <strong>{author.name}</strong> {author.summary}
         {` `}
